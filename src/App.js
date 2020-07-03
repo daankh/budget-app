abedba6b7152ece8e6f0f8daf33c4e61944721e5
daankh <jarosz.dan@gmail.com>
2020-07-03 16:26:34 +0200

    feat: divide routing

--- src/App.js
+++ src/App.js
@@ -1,67 +1,8 @@
 import React from "react";
-import { BrowserRouter, Switch, Route, Link, NavLink } from "react-router-dom";
-
-const ExpenseDashboardPage = () => {
-  return <div>This is from my dashboard component</div>;
-};
-
-const AddExpensePage = () => {
-  return <div>This is from my add expense component</div>;
-};
-
-const EditExpensePage = () => {
-  return <div>This is from my edit component</div>;
-};
-
-const HelpPage = () => {
-  return <div>This is from my help component</div>;
-};
-
-const NotFoundPage = () => {
-  return (
-    <div>
-      404! - <Link to="/">Go home</Link>
-    </div>
-  );
-};
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <nav>
-      <NavLink exact to="/" activeClassName={"is-active"}>
-        Dashboard
-      </NavLink>
-      <NavLink to="/create" activeClassName={"is-active"}>
-        Create Expense
-      </NavLink>
-      <NavLink to="/edit" activeClassName={"is-active"}>
-        Edit Expense
-      </NavLink>
-      <NavLink to="/help" activeClassName={"is-active"}>
-        Help
-      </NavLink>
-    </nav>
-  </header>
-);
-
-const routes = (
-  <BrowserRouter>
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={ExpenseDashboardPage} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
+import AppRouter from "./routers/AppRouter";
 
 const App = () => {
-  return routes;
+  return <AppRouter />;
 };
 
 export default App;
