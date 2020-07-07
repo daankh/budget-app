0869429420feb85620503ff30a5c958b5be508a6
daankh <jarosz.dan@gmail.com>
2020-07-07 23:09:22 +0200

    refactor: clear picker depenencies

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { addExpense, editExpense } from "../../redux/actions/expenses";
+// import DatePicker from "./components/DatePicker";
 
 class ExpenseForm extends Component {
   state = {
