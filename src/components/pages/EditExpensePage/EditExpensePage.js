1a21a36929d388db0a2c6aad57b2d485290daddc
daankh <jarosz.dan@gmail.com>
2020-07-08 07:47:04 +0200

    feat(EditExpensePage): wire up editing action

--- src/components/pages/EditExpensePage/EditExpensePage.js
+++ src/components/pages/EditExpensePage/EditExpensePage.js
@@ -1,14 +1,24 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { editExpense } from "../../../redux/actions/expenses";
 import ExpenseForm from "../../ExpenseForm/ExpenseForm";
 
 const EditExpensePage = () => {
+  const history = useHistory();
   const params = useParams();
+  const dispatch = useDispatch();
+
   const { id } = params;
   return (
     <div>
       <p>Editing the expense with the id {id}</p>
-      <ExpenseForm />
+      <ExpenseForm
+        onSubmit={(id, updates) => {
+          dispatch(editExpense(id, updates));
+          history.push({ pathname: "/" });
+        }}
+      />
     </div>
   );
 };
