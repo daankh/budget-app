998f79e2ff8883b0f75723f110bcf57924a5d6c9
daankh <jarosz.dan@gmail.com>
2020-07-08 07:52:17 +0200

    feat(ExpnseListItem): add linkt to go to edit page

--- src/components/ExpenseList/components/ExpenseListItem/ExpenseListItem.js
+++ src/components/ExpenseList/components/ExpenseListItem/ExpenseListItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { removeExpense } from "../../../../redux/actions/expenses";
 
@@ -6,10 +7,14 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => {
   const dispatch = useDispatch();
   return (
     <div>
-      <h3>{description}</h3>
+      <Link to={`/edit/${id}`}>
+        {" "}
+        <h3>{description}</h3>
+      </Link>
       <p>
         {amount} - {createdAt}
       </p>
+
       <button onClick={() => dispatch(removeExpense({ id }))}>Remove</button>
     </div>
   );
