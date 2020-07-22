0af3b61a88b2d45a66602de8f19ffe6225e38d0d
danielj-rp <danielj@reactpoland.com>
2020-07-22 23:52:18 +0200

    feat: configure firebase with env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDrwMrMXN2xOjmeTM1FGMAVoHQ8mdQ9nzQ",
-    authDomain: "expensify-2c0e1.firebaseapp.com",
-    databaseURL: "https://expensify-2c0e1.firebaseio.com",
-    projectId: "expensify-2c0e1",
-    storageBucket: "expensify-2c0e1.appspot.com",
-    messagingSenderId: "919255393950",
-    appId: "1:919255393950:web:1bcb57e8d8d270d72c7d8f",
-    measurementId: "G-V7TSVCG127"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -16,4 +16,5 @@ firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
 export { firebase, database as default };
+
 
