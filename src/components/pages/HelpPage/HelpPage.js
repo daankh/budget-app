67fb1bdd6c46544faf74194ce47525153deec7f7
danielj-rp <danielj@reactpoland.com>
2020-07-27 19:12:48 +0200

    feat: implement PublicRoute and remove HelpPage

--- src/components/pages/HelpPage/HelpPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const HelpPage = () => {
-  return <div>This is from my help component</div>;
-};
-
-export default HelpPage;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,10 +4,10 @@ import { createBrowserHistory } from "history";
 import ExpenseDashboardPage from "../components/pages/ExpenseDashboardPage/ExpenseDashboardPage";
 import AddExpensePage from "../components/pages/AddExpensePage/AddExpensePage";
 import EditExpensePage from "../components/pages/EditExpensePage/EditExpensePage";
-import HelpPage from "../components/pages/HelpPage/HelpPage";
 import NotFoundPage from "../components/pages/NotFoundPage/NotFoundPage";
 import LoginPage from "../components/pages/LoginPage/LoginPage";
 import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
 
 export const history = createBrowserHistory();
 
@@ -15,11 +15,10 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <Route exact path="/" component={LoginPage} />
+        <PublicRoute exact path="/" component={LoginPage} />
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
         <PrivateRoute path="/create" component={AddExpensePage} />
         <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
