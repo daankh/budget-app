c05a61605ac50278ce72d072251f66f14cc580ae
danielj-rp <danielj@reactpoland.com>
2020-07-28 21:05:14 +0200

    feat(Header): change the structure and adjust tests

--- src/__tests__/components/layout/Header/Header.test.js
+++ src/__tests__/components/layout/Header/Header.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Header from "../../../../components/layout/Header/Header";
-import { NavLink } from "react-router-dom"
 jest.mock("react-redux", () => ({
     ...jest.requireActual("react-redux"),
     useDispatch: () => ({
@@ -33,20 +32,6 @@ describe("Heading", () => {
     })
 })
 
-
-
-test("should render navigation", () => {
-    const wrapper = shallow(<Header />)
-    const component = wrapper.find('[data-test="navigation"]')
-    expect(component.length).toBe(1)
-})
-
-test("should render 2 NavLinks", () => {
-    const wrapper = shallow(<Header />)
-    const component = wrapper.find(NavLink)
-    expect(component.length).toBe(2)
-})
-
 describe("NavLink to dashboard", () => {
     let wrapper = null;
     let component = null;
@@ -60,63 +45,11 @@ describe("NavLink to dashboard", () => {
         expect(component.length).toBe(1)
     })
 
-    test("NavLink to dashboard should have correct text", () => {
-        const expectedText = "Dashboard"
-        expect(component.text()).toBe(expectedText)
-    })
-
     test("should have correct to param", () => {
         const expectedTo = "/dashboard"
         expect(component.prop("to")).toBe(expectedTo)
     })
 })
-
-describe("NavLink to edit page", () => {
-    let wrapper = null;
-    let component = null;
-    beforeEach(() => {
-        wrapper = shallow(<Header />)
-        component = wrapper.find('[data-test="link-create-expense"]')
-    })
-
-    test("should render NavLinkt to dashboard", () => {
-
-        expect(component.length).toBe(1)
-    })
-
-    test("NavLink to edit page should have correct text", () => {
-        const expectedText = "Create Expense"
-        expect(component.text()).toBe(expectedText)
-    })
-
-    test("should have correct to param", () => {
-        const expectedTo = "/create"
-        expect(component.prop("to")).toBe(expectedTo)
-    })
-})
-
-// describe("NavLink to help page", () => {
-//     let wrapper = null;
-//     let component = null;
-//     beforeEach(() => {
-//         wrapper = shallow(<Header />)
-//         component = wrapper.find('[data-test="link-help"]')
-//     })
-
-//     test("should render NavLinkt to help page", () => {
-//         expect(component.length).toBe(1)
-//     })
-
-//     test("NavLink to help page should have correct text", () => {
-//         const expectedText = "Help"
-//         expect(component.text()).toBe(expectedText)
-//     })
-
-//     test("should have correct to param", () => {
-//         const expectedTo = "/help"
-//         expect(component.prop("to")).toBe(expectedTo)
-//     })
-// })
 
 describe("Logout button", () => {
     let wrapper = null;
@@ -128,6 +61,4 @@ describe("Logout button", () => {
         const logoutButton = wrapper.find('[data-test="logout-button"]');
         expect(logoutButton.length).toBe(1);
     });
-
-
 })
