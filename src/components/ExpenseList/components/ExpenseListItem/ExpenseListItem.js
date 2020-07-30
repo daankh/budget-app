22b2150d4aa77b66798f2b977867cecc42408a0f
danielj-rp <danielj@reactpoland.com>
2020-07-30 22:11:34 +0200

    feat(ExpenseList): apply

--- src/components/ExpenseList/components/ExpenseListItem/ExpenseListItem.js
+++ src/components/ExpenseList/components/ExpenseListItem/ExpenseListItem.js
@@ -5,14 +5,15 @@ import numeral from "numeral";
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => {
   return (
-    <div data-test="ExpenseListItem">
-      <Link to={`/edit/${id}`}>
-        <h3>{description}</h3>
-      </Link>
-      <p>
-        {`${numeral(amount / 100).format("$0,0.00")} - ${moment(createdAt).format("DD.MM.YYYY")}`}
-      </p>
-    </div>
+    <Link to={`/edit/${id}`} data-test="ExpenseListItem" className="list-item">
+      <div>
+        <h3 className="list-item__title">{description}</h3>
+        <span className="list-item__subtitle">{moment(createdAt).format("DD.MM.YYYY")}</span>
+      </div>
+      <h3 className="list-item__data">
+        {numeral(amount / 100).format("$0,0.00")}
+      </h3>
+    </Link>
   );
 };
 
